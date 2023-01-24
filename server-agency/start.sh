46b199f0888939123d6259df49fa227b3f768caf
Max Plauth <smaxen@me.com>
2023-01-24 14:20:37 +0100

    Create docker-image.yml (#24)
    * Create docker-image.yml
    
    * unified with cycle4 snapshot
    
    Co-authored-by: Max Plauth <max@ultihash.io>

---
+++ server-agency/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Waiting for uh-server-db to become available..."
+RAND=$(( ( RANDOM % 5 )  + 1 ))
+sleep $RAND
+
+while true
+do
+  STDOUT=$(curl -s -f -i http://uh-server-db:8080/metrics)
+  ERRNO="$?"
+  if [ "$ERRNO" -eq 0 ] ; then
+        echo "uh-server-db has become available, moving on..."
+        break   # end loop
+  fi
+  RAND=$(( ( RANDOM % 5 )  + 5 ))
+  echo "uh-server-db is not available, retrying in $RAND seconds..."
+  sleep $RAND
+done
+
+uhServerAgency
