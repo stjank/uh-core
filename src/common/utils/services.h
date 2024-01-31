5c76f8ef28923a3a46eabe683b88701625df3e86
Stefan Jank <sj@stiftstrasse.de>
2024-01-31 13:12:29 +0100

    Mark test_bucket_listv2_fetchowner_defaultempty as xfail. (#249)

--- src/common/utils/services.h
+++ src/common/utils/services.h
@@ -84,6 +84,7 @@ namespace uh::cluster {
                  m_etcd_client(etcd_host),
                  m_watcher(etcd_host, etcd_services_announced_key_prefix + get_service_string(r),
                           [this](etcd::Response response) {return handle_state_changes(response);}, true),
+                 m_robin_index(m_clients.end()),
                  m_services_index(config_registry)
         {
             wait_for_dependency();
@@ -106,17 +107,20 @@ namespace uh::cluster {
 
         [[nodiscard]] std::shared_ptr <client> get() const
         {
-            auto index = m_nodes_index.load();
-
             std::shared_lock<std::shared_mutex> lk(m_shared_mutex);
 
-            auto new_val = (index + 1) % m_clients.size();
-            while (!m_nodes_index.compare_exchange_weak (index, new_val)) {
-                index = m_nodes_index.load();
-                new_val = (index + 1) % m_clients.size();
+            if (m_clients.empty()) {
+                return std::shared_ptr<client>();
             }
 
-            return m_clients.at(index);
+            if (m_robin_index == m_clients.end()) {
+                m_robin_index = m_clients.begin();
+            }
+
+            auto rv = m_robin_index->second;
+            ++m_robin_index;
+
+            return rv;
         }
 
         [[nodiscard]] std::vector <std::shared_ptr <client>> get_clients() const {
@@ -228,7 +232,18 @@ namespace uh::cluster {
                        << service_endpoint.port ;
 
             std::lock_guard<std::shared_mutex> shared_lk(m_shared_mutex);
-            m_clients.erase(service_endpoint.id);
+            auto it = m_clients.find(service_endpoint.id);
+            if (it == m_clients.end())
+            {
+                return;
+            }
+
+            if (it == m_robin_index) {
+                m_robin_index = m_clients.erase(it);
+            } else {
+                m_clients.erase(it);
+            }
+
             m_services_index.erase(service_endpoint.id);
         }
 
@@ -241,9 +256,8 @@ namespace uh::cluster {
         std::condition_variable_any m_cv;
         std::map <std::size_t, std::shared_ptr <client>> m_clients;
 
-        mutable std::atomic <size_t> m_nodes_index {};
+        mutable std::map<std::size_t, std::shared_ptr<client>>::const_iterator m_robin_index;
         services_index<r> m_services_index;
-
     };
 
 } // end namespace uh::cluster
