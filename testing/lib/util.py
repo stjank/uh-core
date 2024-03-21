d8a74387d76f713d2835be99651d2fc1c03ba10f
MaksimIushchenko <158456131+MaksimIushchenko@users.noreply.github.com>
2024-03-21 16:28:27 +0100

    Mount core-tests repo to testing/ (#369)

--- testing/lib/util.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-
-def random_string(letters=string.ascii_lowercase, length=8):
-   return ''.join(random.choice(letters) for i in range(length))
--- testing/tests/test_test.py
+++
@@ -1,6 +0,0 @@
-#
-# This test case checks whether the test framework is functional.
-#
-
-def test_pytest():
-    assert True
