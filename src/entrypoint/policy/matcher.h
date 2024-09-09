1656760dc1d08a797758a5553f91ab108ac441cb
Stefan Jank <sj@stiftstrasse.de>
2024-09-09 09:40:04 +0200

    Eng 559 condition parser (#511)

--- src/entrypoint/policy/matcher.h
+++ src/entrypoint/policy/matcher.h
@@ -2,6 +2,8 @@
 #define CORE_ENTRYPOINT_POLICY_MATCHER_H
 
 #include <functional>
+#include <list>
+#include <map>
 #include <set>
 #include <string>
 
@@ -12,7 +14,29 @@ class command;
 
 namespace uh::cluster::ep::policy {
 
+enum class undefined_variable { ignore, do_not_match };
+
 typedef std::function<bool(const http_request&, const command&)> matcher;
+
+inline matcher match_always() {
+    return [](const http_request&, const command&) { return true; };
+}
+
+inline matcher match_never() {
+    return [](const http_request&, const command&) { return false; };
+}
+
+inline matcher match_all(std::list<matcher> subs) {
+    return [subs = std::move(subs)](const http_request& r, const command& c) {
+        for (const auto& m : subs) {
+            if (!m(r, c)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+}
 
 inline matcher match_action(std::set<std::string> actions) {
     return [actions = std::move(actions)](const http_request&,
@@ -42,6 +66,272 @@ inline matcher match_principal(std::set<std::string> principals) {
 inline matcher match_not_principal(std::set<std::string> principals) {
     return [actions = std::move(principals)](
                const http_request&, const command& cmd) { return false; };
+}
+
+inline matcher
+match_stringequals(std::map<std::string, std::list<std::string>> strings,
+                   undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request& r,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_stringnotequals(std::map<std::string, std::list<std::string>> strings,
+                      undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_stringequalsignorecase(
+    std::map<std::string, std::list<std::string>> strings,
+    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_stringnotequalsignorecase(
+    std::map<std::string, std::list<std::string>> strings,
+    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_stringlike(std::map<std::string, std::list<std::string>> strings,
+                 undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_stringnotlike(std::map<std::string, std::list<std::string>> strings,
+                    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_numericequals(std::map<std::string, std::list<std::string>> strings,
+                    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_numericnotequals(std::map<std::string, std::list<std::string>> strings,
+                       undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_numericlessthan(std::map<std::string, std::list<std::string>> strings,
+                      undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_numericlessthanequals(
+    std::map<std::string, std::list<std::string>> strings,
+    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_numericgreaterthan(std::map<std::string, std::list<std::string>> strings,
+                         undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_numericgreaterthanequals(
+    std::map<std::string, std::list<std::string>> strings,
+    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_dateequals(std::map<std::string, std::list<std::string>> strings,
+                 undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_datenotequals(std::map<std::string, std::list<std::string>> strings,
+                    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_datelessthan(std::map<std::string, std::list<std::string>> strings,
+                   undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_datelessthanequals(std::map<std::string, std::list<std::string>> strings,
+                         undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_dategreaterthan(std::map<std::string, std::list<std::string>> strings,
+                      undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_dategreaterthanequals(
+    std::map<std::string, std::list<std::string>> strings,
+    undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher match_bool(std::map<std::string, std::list<std::string>> strings,
+                          undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_binaryequals(std::map<std::string, std::list<std::string>> strings,
+                   undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_ipaddress(std::map<std::string, std::list<std::string>> strings,
+                undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_notipaddress(std::map<std::string, std::list<std::string>> strings,
+                   undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_arnequals(std::map<std::string, std::list<std::string>> strings,
+                undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_arnlike(std::map<std::string, std::list<std::string>> strings,
+              undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_arnnotequals(std::map<std::string, std::list<std::string>> strings,
+                   undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_arnnotlike(std::map<std::string, std::list<std::string>> strings,
+                 undefined_variable uv) {
+    return [strings = std::move(strings), uv](const http_request&,
+                                              const command&) {
+        (void)uv;
+        return false;
+    };
+}
+
+inline matcher
+match_null(std::map<std::string, std::list<std::string>> strings) {
+    return [strings = std::move(strings)](const http_request&, const command&) {
+        return false;
+    };
 }
 
 } // namespace uh::cluster::ep::policy
--- src/entrypoint/policy/parser.cpp
+++ src/entrypoint/policy/parser.cpp
@@ -26,6 +26,7 @@ const json& require(const json& j, std::string_view key) {
 
 std::optional<std::reference_wrapper<const json>>
 optional(const json& j, std::string_view key) {
+
     auto it = j.find(key);
     if (it == j.end()) {
         return {};
@@ -34,12 +35,25 @@ optional(const json& j, std::string_view key) {
     return *it;
 }
 
-std::set<std::string> string_or_set(const json& element) {
-    if (element.is_array()) {
-        return std::set<std::string>(element.begin(), element.end());
+std::string to_string(const json& element) {
+    return element.get<std::string>();
+}
+
+template <class container>
+auto multi_element(const json& element, auto reader) {
+    if (!element.is_array()) {
+        return container{reader(element)};
     }
 
-    return {element.get<std::string>()};
+    container rv;
+    for (const auto& sub : element) {
+        rv.insert(rv.end(), reader(sub));
+    }
+    return rv;
+}
+
+std::set<std::string> string_or_set(const json& element) {
+    return multi_element<std::set<std::string>>(element, to_string);
 }
 
 effect get_effect(const json& stmt) {
@@ -105,12 +119,129 @@ std::optional<matcher> principal_matchers(const json& stmt) {
     return {};
 }
 
-std::optional<matcher> condition_matchers(const json& stmt) {
-    if (auto condition = optional(stmt, "Condition"); condition) {
-        // TODO add condition support
+std::map<std::string, std::list<std::string>>
+condition_parameter(const json& condition) {
+    std::map<std::string, std::list<std::string>> rv;
+    for (const auto& elem : condition.items()) {
+        rv[elem.key()] =
+            multi_element<std::list<std::string>>(elem.value(), to_string);
+    }
+    return rv;
+}
+
+matcher condition_matcher(std::string key, const json& condition) {
+    undefined_variable if_exists = undefined_variable::do_not_match;
+    if (key.ends_with("IfExists")) {
+        if_exists = undefined_variable::ignore;
+        key = key.substr(0, key.size() - std::string("IfExists").size());
     }
 
-    return {};
+    if (key == "StringEquals") {
+        return match_stringequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "StringNotEquals") {
+        return match_stringnotequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "StringEqualsIgnoreCase") {
+        return match_stringequalsignorecase(condition_parameter(condition),
+                                            if_exists);
+    }
+    if (key == "StringNotEqualsIgnoreCase") {
+        return match_stringnotequalsignorecase(condition_parameter(condition),
+                                               if_exists);
+    }
+    if (key == "StringLike") {
+        return match_stringlike(condition_parameter(condition), if_exists);
+    }
+    if (key == "StringNotLike") {
+        return match_stringnotlike(condition_parameter(condition), if_exists);
+    }
+    if (key == "NumericEquals") {
+        return match_numericequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "NumericNotEquals") {
+        return match_numericnotequals(condition_parameter(condition),
+                                      if_exists);
+    }
+    if (key == "NumericLessThan") {
+        return match_numericlessthan(condition_parameter(condition), if_exists);
+    }
+    if (key == "NumericLessThanEquals") {
+        return match_numericlessthanequals(condition_parameter(condition),
+                                           if_exists);
+    }
+    if (key == "NumericGreaterThan") {
+        return match_numericgreaterthan(condition_parameter(condition),
+                                        if_exists);
+    }
+    if (key == "NumericGreaterThanEquals") {
+        return match_numericgreaterthanequals(condition_parameter(condition),
+                                              if_exists);
+    }
+    if (key == "DateEquals") {
+        return match_dateequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "DateNotEquals") {
+        return match_datenotequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "DateLessThan") {
+        return match_datelessthan(condition_parameter(condition), if_exists);
+    }
+    if (key == "DateLessThanEquals") {
+        return match_datelessthanequals(condition_parameter(condition),
+                                        if_exists);
+    }
+    if (key == "DateGreaterThan") {
+        return match_dategreaterthan(condition_parameter(condition), if_exists);
+    }
+    if (key == "DateGreaterThanEquals") {
+        return match_dategreaterthanequals(condition_parameter(condition),
+                                           if_exists);
+    }
+    if (key == "Bool") {
+        return match_bool(condition_parameter(condition), if_exists);
+    }
+    if (key == "BinaryEquals") {
+        return match_binaryequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "IpAddress") {
+        return match_ipaddress(condition_parameter(condition), if_exists);
+    }
+    if (key == "NotIpAddress") {
+        return match_notipaddress(condition_parameter(condition), if_exists);
+    }
+    if (key == "ArnEquals") {
+        return match_arnequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "ArnLike") {
+        return match_arnlike(condition_parameter(condition), if_exists);
+    }
+    if (key == "ArnNotEquals") {
+        return match_arnnotequals(condition_parameter(condition), if_exists);
+    }
+    if (key == "ArnNotLike") {
+        return match_arnnotlike(condition_parameter(condition), if_exists);
+    }
+    if (key == "Null") {
+        return match_null(condition_parameter(condition));
+    }
+
+    return match_never();
+}
+
+std::optional<matcher> condition_matchers(const json& stmt) {
+    auto condition = optional(stmt, "Condition");
+    if (!condition) {
+        return {};
+    }
+
+    std::list<matcher> subs;
+
+    for (const auto& elem : condition->get().items()) {
+        subs.emplace_back(condition_matcher(elem.key(), elem.value()));
+    }
+
+    return match_all(std::move(subs));
 }
 
 policy parse_policy(const json& stmt) {
@@ -147,20 +278,11 @@ std::list<policy> parser::parse(const std::string& code) {
         throw std::runtime_error("no version element or unsupported version");
     }
 
-    std::list<policy> rv;
-
     const auto& statements = require(js, "Statement");
     LOG_DEBUG() << "policy parser: " << statements.size()
                 << " policies in Statement";
-    if (statements.is_array()) {
-        for (const auto& stmt : statements) {
-            rv.emplace_back(parse_policy(stmt));
-        }
-    } else {
-        rv.emplace_back(parse_policy(statements));
-    }
 
-    return rv;
+    return multi_element<std::list<policy>>(statements, parse_policy);
 }
 
 } // namespace uh::cluster::ep::policy
