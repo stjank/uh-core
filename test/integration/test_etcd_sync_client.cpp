b696a0f9a16a6ba5a4e5620bd9381672ad48366f
Sungsik Nam <jmyl@me.com>
2025-04-09 14:34:06 +0200

    Eng 1004 test leader election (#726)

--- test/integration/test_etcd_sync_client.cpp
+++ test/integration/test_etcd_sync_client.cpp
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MODULE "etcd tests"
 
-#include <common/etcd/utils.h>
+#include <etcd/KeepAlive.hpp>
+#include <etcd/SyncClient.hpp>
+#include <etcd/Watcher.hpp>
 
 #include "fakeit.hpp"
 
@@ -30,10 +32,21 @@ public:
 
     ~fixture() { etcd_client.rmdir("/", true); }
 
+    void print_ls() {
+        auto resp = etcd_client.ls("/");
+        auto keys = resp.keys();
+        std::map<std::string, std::string> map;
+        for (auto i = 0u; i < keys.size(); ++i) {
+            map[keys[i]] = resp.value(i).as_string();
+        }
+        for (const auto& [key, value] : map) {
+            std::cout << key << ": " << value << std::endl;
+        }
+    }
+
 protected:
     std::string etcd_address;
     etcd::SyncClient etcd_client;
-    etcd_config cfg;
     etcd::Response response;
     Mock<callback_interface> mock;
 };
@@ -156,6 +169,92 @@ BOOST_FIXTURE_TEST_CASE(waits_on_second_lock_until_first_lock_is_unlocked,
     BOOST_TEST(resp_unlock.is_ok());
     auto resp_lock2 = future_lock2.get();
     BOOST_TEST(resp_lock2.is_ok());
+}
+
+BOOST_FIXTURE_TEST_CASE(campaigns_and_observes, fixture) {
+    auto keepalive = etcd_client.leasekeepalive(60);
+    auto lease_id = keepalive->Lease();
+
+    auto observer_thread = std::thread([&]() {
+        std::unique_ptr<etcd::SyncClient::Observer> observer =
+            etcd_client.observe("test");
+
+        // wait many change events, blocked execution
+        for (size_t i = 0; i < 10; ++i) {
+            etcd::Response resp = observer->WaitOnce();
+            std::cout << "observe " << resp.value().key()
+                      << " as the leader: " << resp.value().as_string()
+                      << std::endl;
+        }
+        std::cout << "finish the observe" << std::endl;
+        // cancel the observers
+        observer.reset(nullptr);
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    for (int i = 0; i < 5; ++i) {
+        // campaign
+        auto resp1 = etcd_client.campaign("test", lease_id, "xxxx");
+        BOOST_TEST(0 == resp1.error_code());
+        std::cout << "key " << resp1.value().key() << " becomes the leader"
+                  << std::endl;
+
+        // proclaim
+        auto resp3 = etcd_client.proclaim("test", lease_id, resp1.value().key(),
+                                          resp1.value().created_index(),
+                                          "tttt - " + std::to_string(i));
+        BOOST_TEST(0 == resp3.error_code());
+
+        // leader
+        {
+            auto resp4 = etcd_client.leader("test");
+            BOOST_TEST(0 == resp4.error_code());
+            BOOST_TEST(resp1.value().key() == resp4.value().key());
+            BOOST_TEST("tttt - " + std::to_string(i) ==
+                       resp4.value().as_string());
+        }
+
+        // resign
+        auto resp5 = etcd_client.resign("test", lease_id, resp1.value().key(),
+                                        resp1.value().created_index());
+        BOOST_TEST(0 == resp5.error_code());
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    observer_thread.join();
+}
+
+BOOST_FIXTURE_TEST_CASE(concurrent_campaign, fixture) {
+    auto keepalive = etcd_client.leasekeepalive(60);
+    auto lease_id = keepalive->Lease();
+
+    auto resp1 = etcd_client.campaign("test", lease_id, "1");
+    BOOST_TEST(0 == resp1.error_code());
+    std::clog << "key " << resp1.value().key() << " becomes the leader"
+              << std::endl;
+
+    auto observer_thread = std::thread([&]() {
+        auto keepalive = etcd_client.leasekeepalive(60);
+        auto lease_id = keepalive->Lease();
+        auto resp1 = etcd_client.campaign("test", lease_id, "1");
+        BOOST_TEST(0 == resp1.error_code());
+        std::clog << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
+        std::clog << "Waiting follower becomes the leader" << std::endl;
+        std::clog << "key " << resp1.value().key() << " becomes the leader"
+                  << std::endl;
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // resign
+    auto resp2 = etcd_client.resign("test", lease_id, resp1.value().key(),
+                                    resp1.value().created_index());
+    BOOST_TEST(0 == resp2.error_code());
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    observer_thread.join();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
