f0f6d380abadd4b7e49ebd4a672f44179098a33b
Sungsik Nam <jmyl@me.com>
2025-05-22 23:15:53 +0200

    Compile bug fix

--- src/storage/group/offset.h
+++ src/storage/group/offset.h
@@ -8,8 +8,6 @@
 
 namespace uh::cluster::storage {
 
-using offset_t = std::size_t;
-
 class offset_manager {
 public:
     using callback_t = subscriber::callback_t;
@@ -22,7 +20,7 @@ public:
     ~offset_manager() { m_etcd.rm(m_prefix); }
 
     static void put(etcd_manager& etcd, std::size_t group_id,
-                    std::size_t storage_id, offset_t val) {
+                    std::size_t storage_id, std::size_t val) {
         etcd.put(get_storage_offset_prefix(group_id)[storage_id],
                  serialize(val));
     }
@@ -30,16 +28,19 @@ public:
     auto summarize_offsets(std::chrono::seconds timeout = 2s) {
         auto start_time = std::chrono::steady_clock::now();
 
-        offset_t max_offset = 0;
+        std::size_t max_offset = 0;
+        std::vector<bool> is_read(m_num_storages, false);
         while (true) {
-            auto m_offset_candidates =
-                sync_vector_observer<offset_t>(m_prefix, m_num_storages, -1);
+            auto m_offset_candidates = sync_vector_observer<std::size_t>(
+                m_prefix, m_num_storages, 0,
+                [&is_read](std::size_t id, std::size_t& value) {
+                    is_read[id] = true;
+                });
             reader r("", m_etcd, m_prefix, {m_offset_candidates});
             auto candidates = m_offset_candidates.get();
 
-            bool all_read = std::all_of(
-                candidates.begin(), candidates.end(),
-                [](const auto& candidate) { return candidate != -1; });
+            bool all_read = std::all_of(is_read.begin(), is_read.end(),
+                                        [](const bool val) { return val; });
 
             auto current_time = std::chrono::steady_clock::now();
             if (all_read or current_time - start_time > timeout) {
@@ -53,7 +54,7 @@ public:
             }
         }
 
-        return (max_offset > 0) ? max_offset : 0;
+        return max_offset;
     }
 
 private:
