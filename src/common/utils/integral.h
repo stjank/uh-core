973252b35b5f580e8904a6a9e6a78d849612a1ec
Max Plauth <smaxen@me.com>
2025-06-23 14:08:52 +0200

    ran into an issue where div_ceil behaved odd for x == y

--- src/common/utils/integral.h
+++ src/common/utils/integral.h
@@ -7,7 +7,7 @@ namespace uh::cluster {
 template <typename T> constexpr T div_ceil(T x, T y) {
     static_assert(std::is_integral<T>::value,
                   "div_ceil only supports integral types");
-    return (x + y - 1) / y;
+    return x / y + (x % y != 0);
 }
 
 template <typename T> constexpr T align_up(T x, T y) {
