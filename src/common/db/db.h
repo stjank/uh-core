fa48cde77e85ace235dc26ac0e9622d108cf7296
Stefan Jank <sj@stiftstrasse.de>
2026-05-06 22:27:53 +0200

    More meaningful error messages on DB connect failure

--- src/common/db/db.h
+++ src/common/db/db.h
@@ -34,6 +34,7 @@ inline auto connection_factory(boost::asio::io_context& ioc, const config& cfg,
         LOG_INFO() << "connecting to " << cs;
 
         auto conn = std::make_unique<connection>(ioc, cs);
+        LOG_INFO() << "connection estabilished, query version";
         auto row = conn->raw_exec("SELECT version();");
         LOG_INFO() << "connected to `" << cs << "`: " << *row->string_view(0);
         return conn;
